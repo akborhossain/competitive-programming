a154a99c9834c39aa2db309b90ae51e9cab5fe0e
Md. Akbar Hossain <smakbor33@gmail.com>
2022-11-29 02:05:26 +0600

    Day 1

---
+++ hackerrank/30-Days-of-Code/Day 1 Data Type.java
@@ -0,0 +1,26 @@
+
+
+        /* Declare second integer, double, and String variables. */
+        int num;
+        double dub;
+        String st;
+
+        /* Read and save an integer, double, and String to your variables.*/
+        num=scan.nextInt();
+        dub= scan.nextDouble();
+        scan.nextLine();
+        st=scan.nextLine();
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+num);
+
+        /* Print the sum of the double variables on a new line. */
+        System.out.println(d+dub);
+		
+        /* Concatenate and print the String variables on a new line; 
+        the 's' variable above should be printed first. */
+              
+            
+        System.out.print(s);
+        System.out.println(st);
