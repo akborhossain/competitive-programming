ce4b8232cf09e7995de1e634c7eb8bde025897a8
Md. Akbar Hossain <smakbor33@gmail.com>
2022-12-14 08:30:14 +0600

    Day 22 Binary Search Trees

---
+++ hackerrank/30-Days-of-Code/Day 22 Binary Search Trees.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+	public static int getHeight(Node root){
+      //Write your code here
+      int leftroot=0,rightroot=0;
+      if(root.left!=null){
+          leftroot= getHeight(root.left)+1;
+      }
+      if(root.right!=null){
+          rightroot=getHeight(root.right)+1;
+          
+      }
+      return (rightroot>leftroot?rightroot:leftroot);
+      
+      
+    }
+
+	public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            int height=getHeight(root);
+            System.out.println(height);
+        }	
+}
