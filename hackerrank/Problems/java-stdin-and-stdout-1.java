dfba2a28f9012e50ffe130ea7d7281193b8e5e5f
Md. Akbar Hossain <smakbor33@gmail.com>
2023-02-24 17:41:34 +0600

    java-stdin-and-stdout-1

---
+++ hackerrank/Problems/java-stdin-and-stdout-1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int []a=new int[3];
+        for(int i=0;i<3;i++){
+            
+            a[i] = scan.nextInt();
+        }
+      
+        // Complete this line
+        // Complete this line
+        for(int i=0;i<3;i++)
+        System.out.println(a[i]);
+        // Complete this line
+        // Complete this line
+    }
+}
