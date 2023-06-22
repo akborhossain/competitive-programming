99beb552184e49b650b312bdd855313e2ac80bc6
Md. Akbar Hossain <smakbor33@gmail.com>
2023-06-23 04:13:11 +0600

    udate run time

--- leetcode/problems/290. Word Pattern.py
+++ leetcode/problems/290. Word Pattern.py
@@ -4,4 +4,24 @@ class Solution:
         if len(words)!=len(pattern):
             return False
         return (len(set(pattern))==len(set(words))==len(set(zip(pattern, words))))
+
+# update
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words=s.split(" ")
+        if len(words)!=len(pattern):
+            return False
+        charToword={}
+        wordTochar={}
+        for c,w in zip(pattern,words):
+            if c in charToword and charToword[c]!=w:
+                return False
+            if w in wordTochar and wordTochar[w]!=c:
+                return False
+            charToword[c]=w
+            wordTochar[w]=c
+        return True
+      
+       
+
        
